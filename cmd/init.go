6bdfde809ecdb63350a24cdbec9c3b34c8047d84
Muen Yu <parasomnia97@gmail.com>
2026-02-27 22:17:11 +1100

    feat: support config init

---
+++ cmd/init.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"embed"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const configTemplateFilename = "i18n.jsonc"
+
+//go:embed templates/i18n.jsonc
+var initTemplateFS embed.FS
+
+type initOptions struct {
+	force bool
+}
+
+func newInitCmd() *cobra.Command {
+	o := &initOptions{}
+
+	cmd := &cobra.Command{
+		Use:          "init",
+		Short:        "write the latest i18n.jsonc template",
+		Args:         cobra.NoArgs,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			template, err := initTemplateFS.ReadFile("templates/i18n.jsonc")
+			if err != nil {
+				return fmt.Errorf("read init template: %w", err)
+			}
+
+			if _, err := os.Stat(configTemplateFilename); err == nil && !o.force {
+				return fmt.Errorf("%s already exists; use --force to overwrite", configTemplateFilename)
+			} else if err != nil && !os.IsNotExist(err) {
+				return fmt.Errorf("check %s: %w", configTemplateFilename, err)
+			}
+
+			if err := os.WriteFile(configTemplateFilename, template, 0o644); err != nil {
+				return fmt.Errorf("write %s: %w", configTemplateFilename, err)
+			}
+
+			if _, err := fmt.Fprintf(cmd.OutOrStdout(), "wrote %s\n", configTemplateFilename); err != nil {
+				return fmt.Errorf("write init output: %w", err)
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVar(&o.force, "force", o.force, "overwrite existing i18n.jsonc")
+
+	return cmd
+}
