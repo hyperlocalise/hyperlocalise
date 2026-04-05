8b0c4816e2ca846753d130226ca1e6e4f8e3256f
Minh Cung <cungminh2710@gmail.com>
2026-04-05 20:45:27 +1000

    .

--- apps/hyperlocalise-web/src/app/layout.tsx
+++ apps/hyperlocalise-web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Figtree, Montserrat, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -37,7 +38,9 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-full flex flex-col">
-        <TooltipProvider>{children}</TooltipProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <TooltipProvider>{children}</TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
