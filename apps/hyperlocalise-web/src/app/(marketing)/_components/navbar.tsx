5fb322a60699a4d8de194a47167d556ae25486b1
Minh Cung <cungminh2710@gmail.com>
2026-04-08 08:02:41 +1000

    .

--- apps/hyperlocalise-web/src/app/(marketing)/_components/navbar.tsx
+++ apps/hyperlocalise-web/src/app/(marketing)/_components/navbar.tsx
@@ -85,9 +85,7 @@ export default function Navbar() {
           </Popover>
           {/* Main nav */}
           <div className="flex items-center gap-6">
-            <a href="#" className="text-primary hover:text-primary/90">
-              <Logo />
-            </a>
+            <Logo />
             {/* Navigation menu */}
             <NavigationMenu className="mx-auto max-md:hidden">
               <NavigationMenuList className="gap-2">
