d91e161b98157b354a568f01a46a0e8d1c5f2caf
Minh Cung <cungminh2710@gmail.com>
2026-04-08 08:00:43 +1000

    .

--- apps/hyperlocalise-web/src/app/(marketing)/layout.tsx
+++ apps/hyperlocalise-web/src/app/(marketing)/layout.tsx
@@ -1,10 +1,6 @@
 import Navbar from "./_components/navbar";
 
-export default function MarketingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
