96660ec148fd768da328ef87ee063c7815341e18
Minh Cung <cungminh2710@gmail.com>
2026-04-25 09:28:29 +1000

    feedback

--- apps/hyperlocalise-web/src/components/app/nav-user.tsx
+++ apps/hyperlocalise-web/src/components/app/nav-user.tsx
@@ -39,6 +39,12 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+  const initials = user.name
+    .split(" ")
+    .slice(0, 2)
+    .map((namePart) => namePart[0])
+    .join("")
+    .toUpperCase();
 
   return (
     <SidebarMenu>
@@ -54,7 +60,7 @@ export function NavUser({
           >
             <Avatar className="h-8 w-8 rounded-lg grayscale">
               <AvatarImage src={user.avatar} alt={user.name} />
-              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
               <span className="truncate font-medium">{user.name}</span>
@@ -77,7 +83,7 @@ export function NavUser({
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar className="h-8 w-8 rounded-lg">
                     <AvatarImage src={user.avatar} alt={user.name} />
-                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                    <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
                     <span className="truncate font-medium">{user.name}</span>
@@ -108,7 +114,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem render={<Link href="/auth/sign-out?returnTo=/" />}>
               <HugeiconsIcon icon={Logout01Icon} strokeWidth={2} className="size-4" />
               Log out
             </DropdownMenuItem>
