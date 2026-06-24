3ff05405d285b3f4eadec2e2b4315a0e9a5853d5
Minh Cung <cungminh2710@gmail.com>
2026-06-24 14:11:21 +1000

    sentry

---
+++ apps/hyperlocalise-web/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e8d626aa37a0f72a4f3af93a6c4bac21@o4511618181038080.ingest.us.sentry.io/4511618242904064",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
