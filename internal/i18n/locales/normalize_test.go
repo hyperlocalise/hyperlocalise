6b0a2ecadab94affa80b1c3b22795442d6151070
Minh Cung <cungminh2710@gmail.com>
2026-06-29 09:54:39 +1000

    🧪 Scout: improve locale normalization unit tests (#1141)

--- internal/i18n/locales/normalize_test.go
+++ internal/i18n/locales/normalize_test.go
@@ -36,6 +36,26 @@ func TestNormalizeListSplitsTrimsAndDeduplicates(t *testing.T) {
 			in:   []string{"en", "en-US", "en-GB"},
 			want: []string{"en", "en-US", "en-GB"},
 		},
+		{
+			name: "overlapping values across multiple strings",
+			in:   []string{"en-US, fr-FR", "de-DE, EN-US", "FR-fr, es-ES"},
+			want: []string{"en-US", "fr-FR", "de-DE", "es-ES"},
+		},
+		{
+			name: "first encounter casing wins",
+			in:   []string{"fr-ca", "FR-CA", "Fr-Ca"},
+			want: []string{"fr-ca"},
+		},
+		{
+			name: "various whitespace characters",
+			in:   []string{"en-US\t,\nfr-FR\r,\fde-DE\v"},
+			want: []string{"en-US", "fr-FR", "de-DE"},
+		},
+		{
+			name: "non-breaking space around comma",
+			in:   []string{"en-US\u00A0,\u00A0fr-FR"},
+			want: []string{"en-US", "fr-FR"},
+		},
 	}
 
 	for _, tt := range tests {
