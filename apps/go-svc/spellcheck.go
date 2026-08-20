bf1d7cb2f407e3f06a193330538d5c35ed6bbb7c
QUYNHANHH <quynhanh.ha@student.unimelb.edu.au>
2026-08-21 01:30:18 +1000

    feat(go-svc): add spell-check availability contract

--- apps/go-svc/spellcheck.go
+++ apps/go-svc/spellcheck.go
@@ -1,11 +1,18 @@
 package main
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type SpellingIssue struct {
 	Word        string
 	Suggestions []string
 }
+
+var ErrSpellCheckUnavailable = errors.New("spell checking is not available")
+
+const QAModeSpelling = "spelling"
 
 type SpellChecker interface {
 	Check(ctx context.Context, locale string, words []string) ([]SpellingIssue, error)
@@ -14,5 +21,5 @@ type SpellChecker interface {
 type NoopSpellChecker struct{}
 
 func (NoopSpellChecker) Check(_ context.Context, _ string, _ []string) ([]SpellingIssue, error) {
-	return nil, nil
+	return nil, ErrSpellCheckUnavailable
 }
